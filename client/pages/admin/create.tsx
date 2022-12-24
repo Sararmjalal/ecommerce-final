ae51726aee7640cacf8e1c391269702ed7a62e26
Mmd Ramzi <sara.jalal.115@gmail.com>
2022-12-24 21:06:04 +0330

    create admin loading fixed

--- client/pages/admin/create.tsx
+++ client/pages/admin/create.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useState, useEffect, useLayoutEffect } from 'react'
+import { useState, useLayoutEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { selectAdmin } from '../../global-state/slice'
 import { useMutation } from '@tanstack/react-query'
